a03f091c14ffb03368a1b36838e16885a32cb56b
mirzaromi <mirzaromi12@gmail.com>
2022-04-07 15:38:33 +0700

    menghapus file-file sebelumnya dan mengganti dengan file baru

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('public.index');
-});
-
-Route::get('/admin', function () {
-    return view('admin.index');
+    return view('welcome');
 });
