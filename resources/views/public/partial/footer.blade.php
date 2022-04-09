c010511e002f977476acdc06186d9b26d1c61b82
mirzaromi <mirzaromi12@gmail.com>
2022-04-09 23:52:21 +0700

    penambahan footer

---
+++ resources/views/public/partial/footer.blade.php
@@ -0,0 +1,106 @@
+<footer id="footer-container" class=" enable_footer_columns_dark">
+    <div class="footer-columns">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4"> <span class="jl_none_space"></span>
+                    <div id="disto_about_us_widget-3" class="widget jellywp_about_us_widget">
+                        <div class="widget_jl_wrapper about_widget_content"> <span class="jl_none_space"></span>
+                            <div class="widget-title">
+                                <h2>About us</h2>
+                            </div>
+                            <div class="jellywp_about_us_widget_wrapper">
+                                <p>Mauris mattis auctor cursus. Phasellus tellus tellus, imperdiet ut
+                                    imperdiet eu, iaculis a sem imperdiet ut imperdiet.</p>
+                                <div class="social_icons_widget">
+                                    <ul class="social-icons-list-widget icons_about_widget_display">
+                                        <li><a href="/assets/disto/#" class="youtube" target="_blank"><i
+                                                    class="fa fa-youtube"></i></a>
+                                        </li>
+                                        <li><a href="/assets/disto/#" class="instagram" target="_blank"><i
+                                                    class="fa fa-instagram"></i></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div> <span class="jl_none_space"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4"> <span class="jl_none_space"></span>
+                    <div id="disto_recent_post_widget-3" class="widget post_list_widget">
+                        <div class="widget_jl_wrapper"> <span class="jl_none_space"></span>
+                            <div class="widget-title">
+                                <h2>Get in Touch</h2>
+                            </div>
+                            <div>
+                                <p>Jl. KH. Abdul Chalim No. 1, Kembangbelor, Pacet, Kab. Mojokerto 61374</p>
+                                <p>darasnanet@gmail.com</p>
+                                <p>+62 888-8888-8888</p>
+                            </div> <span class="jl_none_space"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div id="categories-4" class="widget widget_categories">
+                        <div class="widget-title">
+                            <h2>Categories</h2>
+                        </div>
+                        <ul>
+                            <li class="cat-item cat-item-2"><a href="/assets/disto/#"
+                                    title="Sample category description goes here">Active</a> <span>11</span>
+                            </li>
+                            <li class="cat-item cat-item-3"><a href="/assets/disto/#"
+                                    title="Sample category description goes here">Business</a>
+                                <span>10</span>
+                            </li>
+                            <li class="cat-item cat-item-4"><a href="/assets/disto/#"
+                                    title="Sample category description goes here">Crazy</a> <span>5</span>
+                            </li>
+                            <li class="cat-item cat-item-5"><a href="/assets/disto/#"
+                                    title="Sample category description goes here">Gaming</a> <span>10</span>
+                            </li>
+                            <li class="cat-item cat-item-6"><a href="/assets/disto/#"
+                                    title="Sample category description goes here">Health</a> <span>6</span>
+                            </li>
+                            <li class="cat-item cat-item-7"><a href="/assets/disto/#"
+                                    title="Sample category description goes here">Science</a> <span>4</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom enable_footer_copyright_dark">
+        <div class="container">
+            <div class="row bottom_footer_menu_text">
+                <div class="col-md-6 footer-left-copyright">© Copyright 2019 Jellywp. All Rights Reserved
+                    Powered by Jellywp</div>
+                <div class="col-md-6 footer-menu-bottom">
+                    <ul id="menu-footer-menu" class="menu-footer">
+                        <li class="menu-item menu-item-10"><a href="/assets/disto/#">About Us</a>
+                        </li>
+                        <li class="menu-item menu-item-11"><a href="/assets/disto/#">Private policy</a>
+                        </li>
+                        <li class="menu-item menu-item-12"><a href="/assets/disto/#">Forums</a>
+                        </li>
+                        <li class="menu-item menu-item-13"><a href="/assets/disto/#">Community</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- End footer -->
+</div>
+</div>
+<div id="go-top"><a href="/assets/disto/#go-top"><i class="fa fa-angle-up"></i></a>
+</div>
+<script src="/assets/disto/js/jquery.js"></script>
+<script src="/assets/disto/js/fluidvids.js"></script>
+<script src="/assets/disto/js/infinitescroll.js"></script>
+<script src="/assets/disto/js/justified.js"></script>
+<script src="/assets/disto/js/slick.js"></script>
+<script src="/assets/disto/js/theia-sticky-sidebar.js"></script>
+<script src="/assets/disto/js/aos.js"></script>
+<script src="/assets/disto/js/custom.js"></script>
