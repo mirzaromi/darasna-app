33e019f59f1a1d4a13c2289a6fdebeddbb1ef491
mirzaromi <mirzaromi12@gmail.com>
2022-04-16 00:08:57 +0700

    memperbaiki tampilan komen mengurangi margin bottom

--- resources/views/public/post/display_comment.blade.php
+++ resources/views/public/post/display_comment.blade.php
@@ -5,20 +5,20 @@
         <strong>{{ $comment->reply_user }}</strong>
         <p>{{ $comment->isi }}</p>
         <a href="" id="reply"></a>
-        <form method="post" action="{{ route('comment.store') }}">
+        <form method="post" action="{{ route('comment.store') }}" style="margin-bottom: 0px">
             @csrf
             <div class="form-group">
-                <input type="text" name="user" class="form-control" placeholder="Nama" style="width: 30%" />
-                <br>
+                <input type="text" name="user" class="form-control" placeholder="Nama" style="width: 30%; margin-bottom: 10px" />
+                
                 <input type="text" name="isi" class="form-control" placeholder="Isi"/>
                 <input type="hidden" name="post_id" value="{{ $post_id }}" />
                 <input type="hidden" name="parent_id" value="{{ $comment->id }}" />
                 <input type="hidden" name="reply_user" value="{{ $comment->user }}" />
             </div>
-            <div class="form-group">
+            <div class="form-group" style="margin-bottom: 20px">
                 <input type="submit" class="btn btn-success" value="Reply" />
             </div>
-            <hr />
+            
         </form>
     </div>
     @include('public.post.display_comment', ['comments' => $comment->reply])
