9339c93445df5c0bfb5bc66583210ca573acaf79
mirzaromi <mirzaromi12@gmail.com>
2022-04-20 03:26:16 +0700

    penambahan fitur instagram feed otomatis

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,27 +4,50 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Phpfastcache\Helper\Psr16Adapter;
 
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $akidah = Post::where('kategori','AKIDAH')->latest()->take(1)->get();
-        $hukum = Post::where('kategori','HUKUM')->latest()->take(1)->get();
-        $sejarah = Post::where('kategori','SEJARAH')->latest()->take(1)->get();
-        $tokoh = Post::where('kategori','TOKOH')->latest()->take(1)->get();
+        $akidah = Post::where('kategori', 'AKIDAH')->latest()->take(1)->get();
+        $hukum = Post::where('kategori', 'HUKUM')->latest()->take(1)->get();
+        $sejarah = Post::where('kategori', 'SEJARAH')->latest()->take(1)->get();
+        $tokoh = Post::where('kategori', 'TOKOH')->latest()->take(1)->get();
         $kategori = Post::select('kategori')->distinct()->get();
-        $carousel_post = array($akidah[0],$hukum[0],$sejarah[0],$tokoh[0]);
+        $carousel_post = array($akidah[0], $hukum[0], $sejarah[0], $tokoh[0]);
         // dd($carousel_post);
-        
-        return view('public.index',[
+
+        $instagram = \InstagramScraper\Instagram::withCredentials(new \GuzzleHttp\Client(), 'mai.amanatulummah.pacet', 'Suksesptn01', new Psr16Adapter('Files'));
+        $instagram->login();
+        $instagram->saveSession();
+        $account = $instagram->getAccount('darasna.network');
+        $accountMedias = $account->getMedias();
+        // dd($accountMedias);
+        // dd($posts[2]->getImageHighResolutionUrl());
+        // dd($accountMedias[0]->getImageHighResolutionUrl());
+        // dd($accountMedias);
+        foreach ($accountMedias as $key => $accountMedia) 
+        {
+            // dump($key);
+            $images[$key] = str_replace("&amp;","&",$accountMedia->getImageHighResolutionUrl());
+            // dump($images[$key]);
+            $path = $accountMedia->getImageHighResolutionUrl();
+            $imageName = $key.'.png';
+            $img = public_path('insta/images'. $imageName);
+            // dump($img);
+            file_put_contents($img, file_get_contents($path));
+        }
+        // die();
+        return view('public.index', [
             'title1' => 'Home',
             'carousel_posts' => $carousel_post,
             'posts_latest' => Post::latest()->take(6)->get(),
             'popular_post' => Post::orderBy('like', 'desc')->take(1)->get(),
             'other_pop_post' => Post::orderBy('like', 'desc')->skip(1)->take(4)->get(),
             'kategori' => $kategori,
+            'instagram' => $images,
         ]);
     }
 }
--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <!--[if IE 9 ]>
 <html class="ie ie9" lang="en-US">
@@ -271,13 +272,15 @@
                         class="jelly_homepage_builder jl_nonav_margin homepage_builder_3grid_post jl_fontsize18 jl_cus_grid4   colstyle1    ">
                         <div class="jl_wrapper_row jl-post-block-111621">
                             <div class="row">
+                                @foreach ($instagram  as $key => $image)
+                                    
                                 <div class="col-md-4 blog_grid_post_style  jl_row_1">
                                     <div class="jl_grid_box_wrapper">
                                         <div class="image-post-thumb">
                                             <a href="/assets/disto/#" class="link_image featured-thumbnail"
                                                 title="This place really good place for reading">
-                                                <img width="400" height="280" src="/assets/disto/img/400x280.png"
-                                                    class="attachment-disto_slider_grid_small size-disto_slider_grid_small wp-post-image"
+                                                <img width="400" height="280" src="{{url('insta/')}}/{{'images'.$key.'.png'}}"
+                                                    class="attachment-disto_slider_grid_small size-disto_slider_grid_small wp-post-image img_insta"
                                                     alt="" />
                                                 <div class="background_over_image"></div>
                                             </a> <span class="meta-category-small"><a class="post-category-color-text"
@@ -298,88 +301,8 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-md-4 blog_grid_post_style  jl_row_2">
-                                    <div class="jl_grid_box_wrapper">
-                                        <div class="image-post-thumb">
-                                            <a href="/assets/disto/#" class="link_image featured-thumbnail"
-                                                title="Having fun with DJ and the best music drop">
-                                                <img width="400" height="280" src="/assets/disto/img/400x280.png"
-                                                    class="attachment-disto_slider_grid_small size-disto_slider_grid_small wp-post-image"
-                                                    alt="" />
-                                                <div class="background_over_image"></div>
-                                            </a> <span class="meta-category-small"><a class="post-category-color-text"
-                                                    style="background:#36c942" href="/assets/disto/#">Sports</a></span>
-                                            <span class="jl_post_type_icon"><i class="la la-camera"></i></span>
-                                        </div>
-                                        <div class="post-entry-content">
-                                            <h3 class="image-post-title"><a href="/assets/disto/#">
-                                                    Having fun with DJ and the best music drop</a>
-                                            </h3>
-                                            <span class="jl_post_meta"><span class="jl_author_img_w"> <img
-                                                        src="/assets/disto/img/favicon.jpg" width="30" height="30"
-                                                        alt="Anna Nikova"
-                                                        class="avatar avatar-30 wp-user-avatar wp-user-avatar-30 alignnone photo" /><a
-                                                        href="/assets/disto/#" title="Posts by Anna Nikova"
-                                                        rel="author">Anna
-                                                        Nikova</a></span><span class="post-date"><i
-                                                        class="fa fa-clock-o"></i>Dec 24, 2016</span></span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-4 blog_grid_post_style  jl_row_3">
-                                    <div class="jl_grid_box_wrapper">
-                                        <div class="image-post-thumb">
-                                            <a href="/assets/disto/#" class="link_image featured-thumbnail"
-                                                title="This guitar sound is so good and i need it more">
-                                                <img width="400" height="280" src="/assets/disto/img/400x280.png"
-                                                    class="attachment-disto_slider_grid_small size-disto_slider_grid_small wp-post-image"
-                                                    alt="" />
-                                                <div class="background_over_image"></div>
-                                            </a> <span class="meta-category-small"><a class="post-category-color-text"
-                                                    style="background:#d800f9" href="/assets/disto/#">Techno</a></span>
-                                        </div>
-                                        <div class="post-entry-content">
-                                            <h3 class="image-post-title"><a href="/assets/disto/#">
-                                                    This guitar sound is so good and i need it more</a>
-                                            </h3>
-                                            <span class="jl_post_meta"><span class="jl_author_img_w"> <img
-                                                        src="/assets/disto/img/favicon.jpg" width="30" height="30"
-                                                        alt="Anna Nikova"
-                                                        class="avatar avatar-30 wp-user-avatar wp-user-avatar-30 alignnone photo" /><a
-                                                        href="/assets/disto/#" title="Posts by Anna Nikova"
-                                                        rel="author">Anna
-                                                        Nikova</a></span><span class="post-date"><i
-                                                        class="fa fa-clock-o"></i>Dec 24, 2016</span></span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-4 blog_grid_post_style  jl_row_4">
-                                    <div class="jl_grid_box_wrapper">
-                                        <div class="image-post-thumb">
-                                            <a href="/assets/disto/#" class="link_image featured-thumbnail"
-                                                title="Technology can make your live easy and fast">
-                                                <img width="400" height="280" src="/assets/disto/img/400x280.png"
-                                                    class="attachment-disto_slider_grid_small size-disto_slider_grid_small wp-post-image"
-                                                    alt="" />
-                                                <div class="background_over_image"></div>
-                                            </a> <span class="meta-category-small"><a class="post-category-color-text"
-                                                    style="background:#d800f9" href="/assets/disto/#">Techno</a></span>
-                                        </div>
-                                        <div class="post-entry-content">
-                                            <h3 class="image-post-title"><a href="/assets/disto/#">
-                                                    Technology can make your live easy and fast</a>
-                                            </h3>
-                                            <span class="jl_post_meta"><span class="jl_author_img_w"> <img
-                                                        src="/assets/disto/img/favicon.jpg" width="30" height="30"
-                                                        alt="Anna Nikova"
-                                                        class="avatar avatar-30 wp-user-avatar wp-user-avatar-30 alignnone photo" /><a
-                                                        href="/assets/disto/#" title="Posts by Anna Nikova"
-                                                        rel="author">Anna
-                                                        Nikova</a></span><span class="post-date"><i
-                                                        class="fa fa-clock-o"></i>Dec 24, 2016</span></span>
-                                        </div>
-                                    </div>
-                                </div>
+                                @endforeach
+
                                 <div class="clear_line_3col_home"></div>
                             </div>
                         </div>
